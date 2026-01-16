d95b722da9d899f89c98e17b6fc32a330d6215ab
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 07:20:38 -0600

    2026-01-16 07:20 I've been at it over six hours with a broken arm. I think I'll take a break... ;)

---
+++ src/main/java/io/github/msobkow/v3_1/cfint/cfintcustfx/CFIntCustMainPane.java
@@ -0,0 +1,172 @@
+// Description: Java 13 Cust JavaFX Schema.
+
+/*
+ *	io.github.msobkow.CFInt
+ *
+ *	Copyright (c) 2020-2025 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFInt - Internet Essentials
+ *	
+ *	This file is part of Mark's Code Fractal CFInt.
+ *	
+ *	Mark's Code Fractal CFInt is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *	
+ *	Mark's Code Fractal CFInt is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFInt is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFInt.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ *	
+ */
+
+package io.github.msobkow.v3_1.cfint.cfintcustfx;
+
+import javafx.geometry.Orientation;
+import javafx.scene.layout.AnchorPane;
+
+import io.github.msobkow.v3_1.cflib.*;
+import io.github.msobkow.v3_1.cflib.dbutil.*;
+import io.github.msobkow.v3_1.cflib.xml.*;
+import io.github.msobkow.v3_1.cflib.javafx.*;
+import io.github.msobkow.v3_1.cfsec.cfseccustfx.*;
+
+public class CFIntCustMainPane
+extends AnchorPane
+implements ICFSecAuthorizationCallbacks
+{
+	protected ICFIntCustSchema custSchema = null;
+	protected CFSplitPane splitPane = null;
+	protected CFTitledPane appConsoleTitledPane = null;
+	protected CFConsole appConsole = null;
+	protected CFTabPane tabPane = null;
+	protected CFTabFormManager tabSec = null;
+	protected CFTabFormManager tabInt = null;
+	protected CFSecCustFacetPane paneSecFacet = null;
+	protected CFIntCustFacetPane paneIntFacet = null;
+
+	public CFIntCustMainPane(
+		ICFIntCustSchema argSchema )
+	{
+		super();
+		custSchema = argSchema;
+		setMinHeight( 480 );
+		setMinWidth( 800 );
+		splitPane = new CFSplitPane();
+
+		appConsoleTitledPane = new CFTitledPane();
+		appConsoleTitledPane.setText( "Console Log" );
+
+		appConsole = new CFConsole();
+		appConsole.setMinHeight( 60 );
+		appConsoleTitledPane.setContent( appConsole );
+
+		tabPane = new CFTabPane();
+
+		tabSec = new CFTabFormManager() {
+			public void showRootMainForm() {
+				if( paneSecFacet == null ) {
+					paneSecFacet = new CFSecCustFacetPane( tabSec, argSchema );
+				}
+				tabSec.setRootForm( paneSecFacet );
+			}
+		};
+		tabSec.setText( "Sec" );
+		tabSec.setClosable( false );
+		tabSec.setContent( paneSecFacet );
+		tabPane.getTabs().add( tabSec );
+
+		if( paneSecFacet == null ) {
+			paneSecFacet = new CFSecCustFacetPane( tabSec, argSchema );
+		}
+		paneSecFacet.setAuthorizationCallbacks( this );
+
+		tabInt = new CFTabFormManager() {
+			public void showRootMainForm() {
+				if( paneIntFacet == null ) {
+					paneIntFacet = new CFIntCustFacetPane( tabInt, argSchema );
+				}
+				tabInt.setRootForm( paneIntFacet );
+			}
+		};
+
+		tabInt.setText( "Int" );
+		tabInt.setClosable( false );
+		tabInt.setContent( paneIntFacet );
+		tabInt.setDisable( true );
+		tabPane.getTabs().add( tabInt );
+		paneIntFacet = new CFIntCustFacetPane( tabInt, argSchema );
+		tabInt.setRootForm( paneIntFacet );
+		splitPane.setOrientation( Orientation.VERTICAL );
+		splitPane.getItems().add( tabPane );
+		splitPane.getItems().add( appConsoleTitledPane );
+		setTopAnchor( splitPane, 0.0 );
+		setLeftAnchor( splitPane, 0.0 );
+		setRightAnchor( splitPane, 0.0 );
+		setBottomAnchor( splitPane, 0.0 );
+		getChildren().addAll( splitPane );
+		tabInt.setDisable( true );
+
+		paneIntFacet.showFacet();
+		if( custSchema.getSchema().getAuthorization() != null ) {
+			paneSecFacet.loggedIn();
+			paneSecFacet.showSession();
+		}
+		else {
+			paneSecFacet.showLogin();
+		}
+	}
+
+	public ICFIntCustSchema getCustSchema() {
+		return( custSchema );
+	}
+
+	public void setCustSchema( ICFIntCustSchema argSchema ) {
+		final String S_ProcName = "setCustSchema";
+		final String S_ArgNameSchema = "argSchema";
+		if( argSchema == null ) {
+			throw new CFLibNullArgumentException( getClass(),
+				S_ProcName,
+				1,
+				S_ArgNameSchema );
+		}
+		if( ! ( argSchema instanceof ICFIntCustSchema ) ) {
+			throw new CFLibUnsupportedClassException( getClass(),
+				S_ProcName,
+				S_ArgNameSchema,
+				argSchema,
+				"ICFIntCustSchema" );
+		}
+		custSchema = (ICFIntCustSchema)argSchema;
+		if( paneSecFacet != null ) {
+			paneSecFacet.setCustSchema( custSchema );
+		}
+		if( paneIntFacet != null ) {
+			paneIntFacet.setCustSchema( custSchema );
+		}
+	}
+
+	public void loggedIn() {
+		tabSec.setDisable( false );
+		tabInt.setDisable( false );
+		paneIntFacet.showFacet();
+	}
+
+	public void preLogout() {
+		tabSec.setDisable( false );
+		tabInt.setDisable( true );
+		paneIntFacet.showFacet();
+	}
+}
---
+++ src/main/java/io/github/msobkow/v3_1/cfint/cfintcustfx/ICFIntCustSchema.java
@@ -0,0 +1,61 @@
+// Description: Java 13 Cust JavaFX Schema Interface.
+
+/*
+ *	io.github.msobkow.CFInt
+ *
+ *	Copyright (c) 2020-2025 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFInt - Internet Essentials
+ *	
+ *	This file is part of Mark's Code Fractal CFInt.
+ *	
+ *	Mark's Code Fractal CFInt is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *	
+ *	Mark's Code Fractal CFInt is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFInt is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFInt.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ *	
+ */
+
+package io.github.msobkow.v3_1.cfint.cfintcustfx;
+
+import io.github.msobkow.v3_1.cflib.*;
+import io.github.msobkow.v3_1.cflib.dbutil.*;
+import io.github.msobkow.v3_1.cflib.xml.*;
+import io.github.msobkow.v3_1.cflib.javafx.*;
+import io.github.msobkow.v3_1.cfsec.cfseccustfx.*;
+import io.github.msobkow.v3_1.cfint.cfintobj.*;
+import io.github.msobkow.v3_1.cfint.cfintjavafx.*;
+
+/**
+ *	The ICFIntSwingSchema defines the interface for the shared schema
+ *	object that is bound to a client.  The various implementations of
+ *	facets cast the shared CFIntSchemaObj as required to access their
+ *	data.
+ *	<p>
+ *	The formats of the configuration files don't change between applications,
+ *	so only the CFInt-specified implementation is used.  It has all the
+ *	attributes needed to set up client-server database connections (not that I
+ *	code to that style any more.)
+ */
+public interface ICFIntCustSchema
+extends ICFSecCustSchema
+{
+	CFIntCustFacetPane newIntFacetPane( ICFFormManager formManager );
+	CFIntCustFacetPane newIntFacetPane( ICFFormManager formManager, ICFIntCustSchema argSchema );
+}
